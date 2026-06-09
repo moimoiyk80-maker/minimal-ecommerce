964e9ff8839d337d64298388d6c2b15190a10785
Hwang Yoon-kyung <moimoiyk80@gmail.com>
2026-06-09 10:44:19 +0900

    readme

--- src/data/products.js
+++ src/data/products.js
@@ -1,82 +1,82 @@
 export const products = [
     {
       id: 1,
-      name: "오버핏 셔츠",
+      name: "시그니처 숏팬츠",
       price: 39000,
-      category: "상의",
+      category: "하의",
       color: "black",
       image: "/images/products/product1.jpg"
     },
     {
       id: 2,
-      name: "와이드 슬랙스",
-      price: 59000,
-      category: "하의",
-      color: "white",
+      name: "오버 핏 반팔 티셔츠",
+      price: 28000,
+      category: "상의",
+      color: "gray",
       image: "/images/products/product2.jpg"
     },
     {
       id: 3,
-      name: "기본 반팔 티셔츠",
-      price: 19000,
+      name: "남녀 공용자켓",
+      price: 59000,
       category: "상의",
-      color: "gray",
+      color: "black",
       image: "/images/products/product3.jpg"
     },
     {
       id: 4,
-      name: "오버핏 셔츠",
-      price: 39000,
-      category: "상의",
+      name: "밴딩 팬츠",
+      price: 49000,
+      category: "하의",
       color: "black",
       image: "/images/products/product4.jpg"
     },
     {
       id: 5,
-      name: "와이드 슬랙스",
-      price: 59000,
-      category: "하의",
-      color: "white",
+      name: "오버핏 반팔 셔츠",
+      price: 29000,
+      category: "상의",
+      color: "gray",
       image: "/images/products/product5.jpg"
     },
     {
       id: 6,
-      name: "기본 반팔 티셔츠",
-      price: 19000,
-      category: "하의",
-      color: "gray",
+      name: "기본 긴팔 셔츠",
+      price: 49000,
+      category: "상의",
+      color: "white",
       image: "/images/products/product6.jpg"
     },
     {
       id: 7,
       name: "와이드 슬랙스",
       price: 59000,
-      category: "상의",
-      color: "gray",
+      category: "하의",
+      color: "black",
       image: "/images/products/product7.jpg"
     },
     {
       id: 8,
-      name: "기본 반팔 티셔츠",
+      name: "니트 반팔 티셔츠",
       price: 19000,
-      category: "하의",
-      color: "black",
+      category: "상의",
+      color: "white",
       image: "/images/products/product8.jpg"
     },
     {
       id: 9,
-      name: "와이드 슬랙스",
-      price: 59000,
-      category: "상의",
+      name: "와이드 밴딩 팬츠",
+      price: 39000,
+      category: "하의",
       color: "gray",
       image: "/images/products/product9.jpg"
     },
     {
       id: 10,
-      name: "기본 반팔 티셔츠",
-      price: 19000,
-      category: "하의",
-      color: "black",
+      name: "버튼 블라우스",
+      price: 69000,
+      category: "상의",
+      color: "gray",
       image: "/images/products/product10.jpg"
     }
   ];
